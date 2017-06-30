50ff2e9ddb71c5792fdbb02a892d178aa533154f
chandrasekarvelraj <vchandras@mindfiresolutions.com>
2017-06-30 18:09:57 +0530

    transaction added in module

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,7 +6,7 @@ class FriendshipsController < ApplicationController
 
   def create
     @friend_id = params[:friend_id]
-    if Friendship.add_friend(current_user.id, params[:friend_id]) &&  Friendship.add_friend(params[:friend_id], current_user.id)
+    if Friendship.add_friend(current_user.id, params[:friend_id])
       respond_to do |format|
         format.html
         format.js {  flash.now[:notice] = "Added Friend Successfully" }
@@ -24,13 +24,13 @@ class FriendshipsController < ApplicationController
   def destroy
     friendship = current_user.friendships.find(params[:id])
     @friendship_id = params[:id]
-    if Friendship.remove_friend(friendship.friend_id, current_user.id) && Friendship.remove_friend(current_user.id, friendship.friend_id)
+    if Friendship.remove_friend(friendship.friend_id, current_user.id)
       respond_to do |format|
         format.html
         format.js {  flash.now[:notice] = "Friend Removed successfully" }
       end
     else
-      flash[:error] = "Error occurred when adding friend."
+      flash[:error] = "Error occurred when removing friend."
       redirect_to root_url
     end  
   end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,13 +3,27 @@ class Friendship < ActiveRecord::Base
   belongs_to :friend, :class_name => "User"
 
   def self.add_friend(first_id, second_id)
-    friend = self.new(:user_id => first_id, :friend_id => second_id)
-    true if friend.save rescue false
+    begin
+      self.transaction do 
+        self.create(:user_id => first_id, :friend_id => second_id)
+        self.create(:user_id => second_id, :friend_id => first_id)
+      end
+      return true
+    rescue ActiveRecord::RecordInvalid
+      return false
+    end
   end
 
   def self.remove_friend(first_id, second_id)
-    friend = self.find_by user_id: first_id, friend_id: second_id
-    true if friend.destroy rescue false
+    begin
+      self.transaction do 
+        self.where(:user_id => first_id, :friend_id => second_id).destroy_all
+        self.where(:user_id => second_id, :friend_id => first_id).destroy_all
+      end
+      return true
+    rescue ActiveRecord::RecordInvalid
+      return false
+    end
   end
 
 end
